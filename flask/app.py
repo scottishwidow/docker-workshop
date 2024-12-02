a70b3ab1e79b6bc1cd8784fb0d637b353a7901d4
scottishwidow <mike.voznyanskix@gmail.com>
2024-12-02 15:52:38 +0200

    records added | gunicorn added

--- flask/app.py
+++ flask/app.py
@@ -1,29 +1,38 @@
 import os
-from flask import Flask
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/info', methods=['GET'])
+@app.route('/healthz', methods=['GET'])
+def about():
+    healthz = 'Application is running!'
 
-def info():
-    health = 'Application is running!'
+    return {'healthz': healthz}, 200
 
-    return {'health': health }, 200
 
-@app.route('/env', methods=['GET'])
+@app.route('/environment', methods=['GET'])
+def secrets():
+    creds = dict()
 
-def env():
-    value = os.environ.get('ENV_VALUE')
+    creds['env-1'] = os.environ.get('ENV_VALUE')
+    creds['env-2'] = os.environ.get('ENV_TOKEN')
 
-    return {'value': value}, 200
+    return creds, 200
 
-@app.route('/creds', methods=['GET'])
 
-def creds():
-    values = dict()
+@app.route('/records', methods=['GET', 'POST'])
+def volumes():
+    filename = '/data/test'
 
-    values['secure'] = open("/run/secrets/secret-1", "r").read()
-    values['insecure'] = os.environ.get('ENV_TOKEN')
+    if request.method == 'POST':
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename, 'w') as f:
+            f.write('Customer record')
 
-    return values, 200
+        return 'Saved!', 201
+    else:
+        f = open(filename, 'r')
+
+        return f.read(), 200
