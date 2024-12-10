243d57fcb52afcf54758690e709a6072fed04a42
scottishwidow <mike.voznyanskix@gmail.com>
2024-12-10 17:36:26 +0200

    dev mode added

--- node/src/index.js
+++ node/src/index.js
@@ -9,43 +9,43 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-let pool;
-try {
-    const dbHost = process.env.DB_HOST;
-    const dbDatabase = process.env.DB_DATABASE;
-    const dbUser = process.env.DB_USER;
-    const dbPassword = fs.readFileSync('/run/secrets/pg-password', 'utf-8').trim();
-
-    pool = new Pool({
-        host: dbHost,
-        database: dbDatabase,
-        user: dbUser,
-        password: dbPassword,
-    });
-
-    pool.connect(async (err, client, release) => {
-        if (err) {
-            console.error('Failed to connect to the database:', err);
-            throw err;
-        }
-
-        console.info('Successfully connected to the database.');
-
-        await client.query(`
-            CREATE TABLE IF NOT EXISTS records (
-                id SERIAL PRIMARY KEY,
-                content TEXT NOT NULL
-            )
-        `);
-        release();
-    });
-} catch (err) {
-    console.error('Database connection error:', err);
-}
+// let pool;
+// try {
+//     const dbHost = process.env.DB_HOST;
+//     const dbDatabase = process.env.DB_DATABASE;
+//     const dbUser = process.env.DB_USER;
+//     const dbPassword = fs.readFileSync('/run/secrets/pg-password', 'utf-8').trim();
+//
+//     pool = new Pool({
+//         host: dbHost,
+//         database: dbDatabase,
+//         user: dbUser,
+//         password: dbPassword,
+//     });
+//
+//     pool.connect(async (err, client, release) => {
+//         if (err) {
+//             console.error('Failed to connect to the database:', err);
+//             throw err;
+//         }
+//
+//         console.info('Successfully connected to the database.');
+//
+//         await client.query(`
+//             CREATE TABLE IF NOT EXISTS records (
+//                 id SERIAL PRIMARY KEY,
+//                 content TEXT NOT NULL
+//             )
+//         `);
+//         release();
+//     });
+// } catch (err) {
+//     console.error('Database connection error:', err);
+// }
 
 
 app.get('/healthz', (req, res) => {
-    res.status(200).json({ healthz: 'Application is running!' });
+    res.status(200).json({ healthz: 'Application is running!!!' });
 });
 
 app.route('/volumes')
@@ -78,33 +78,33 @@ app.get('/environment', (req, res) => {
     });
 });
 
-app.route('/records')
-    .get(async (req, res) => {
-        try {
-            const result = await pool.query('SELECT id, content FROM records');
-            res.status(200).json(result.rows);
-        } catch (err) {
-            console.error('Error fetching records:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    })
-    .post(async (req, res) => {
-        const { content } = req.body;
-        if (!content) {
-            return res.status(400).json({ error: 'Content is required' });
-        }
-
-        try {
-            const result = await pool.query(
-                'INSERT INTO records (content) VALUES ($1) RETURNING id',
-                [content]
-            );
-            res.status(201).json({ message: 'Record inserted successfully', id: result.rows[0].id });
-        } catch (err) {
-            console.error('Error inserting record:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    });
+// app.route('/records')
+//     .get(async (req, res) => {
+//         try {
+//             const result = await pool.query('SELECT id, content FROM records');
+//             res.status(200).json(result.rows);
+//         } catch (err) {
+//             console.error('Error fetching records:', err);
+//             res.status(500).json({ error: 'Internal Server Error' });
+//         }
+//     })
+//     .post(async (req, res) => {
+//         const { content } = req.body;
+//         if (!content) {
+//             return res.status(400).json({ error: 'Content is required' });
+//         }
+//
+//         try {
+//             const result = await pool.query(
+//                 'INSERT INTO records (content) VALUES ($1) RETURNING id',
+//                 [content]
+//             );
+//             res.status(201).json({ message: 'Record inserted successfully', id: result.rows[0].id });
+//         } catch (err) {
+//             console.error('Error inserting record:', err);
+//             res.status(500).json({ error: 'Internal Server Error' });
+//         }
+//     });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
